489b84f4dfaad2e9faf48cb8fbbbf9df12033288
Kosuke Komiya <kosuke.komiya@gmail.com>
2020-04-06 23:28:56 +0900

    Implement a procedure from the book.

--- chapter1/factorial-reverse.scm
+++ chapter1/factorial-reverse.scm
@@ -7,12 +7,24 @@
   (define (iter counter)
     (if (= (+ counter) n)
         n
-        (* (+ counter) (iter (+ counter 1)))))
+        (* counter (iter (+ counter 1)))))
 
   (iter 1))
+
+(define (factorial-2 n)
+  (fact-iter 1 1 n))
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+                 (+ counter 1)
+                 max-count)))
 
 (factorial 1)
 (factorial 6)
 
 (r-factorial 1)
 (r-factorial 6)
+
+(factorial-2 1)
+(factorial-2 6)
