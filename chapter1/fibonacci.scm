6cbd9fb1e2e9375e9c651cd495594a118d64d00c
Kosuke Komiya <kosuke.komiya@gmail.com>
2020-05-18 02:22:03 +0900

    Implement the fibonacci method.

---
+++ chapter1/fibonacci.scm
@@ -0,0 +1,13 @@
+(define (fib n)
+  (cond ((= 0 n) 0)
+        ((= 1 n) 1)
+        (else (+ (fib (- n 1)) (fib (- n 2))))))
+
+(fib 0)
+(fib 1)
+(fib 2)
+(fib 3)
+(fib 4)
+(fib 5)
+(fib 6)
+(fib 7)
