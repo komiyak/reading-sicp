0cb7897052221b463d05ffdbd4a5c22dc278d3ea
Kosuke Komiya <kosuke.komiya@gmail.com>
2020-07-17 17:07:36 +0900

    Answer the question 1.11 of a iterative process. But it's wrong.

--- chapter1/q1-11/answer.scm
+++ chapter1/q1-11/answer.scm
@@ -1,7 +1,7 @@
 ; n >= 3 のとき, f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3)
 ; n < 3 のとき,  f(n) = n
 
-; 再帰的プロセスの実装
+; recursive process の実装
 (define (f n)
   (cond ((< n 3) n)
         (else (+ (f (- n 1))
@@ -11,3 +11,19 @@
 (f 2) ; => 2
 (f 3) ; => 4
 (f 4) ; => 11
+
+
+
+(define (iter total count n)
+  (cond ((= count 0) total)
+        ((>= n 3) (iter total 3 (- n 1))) ; n >= 3 のとき
+        (else (iter (+ total (* count (- count n))) ; ここで f(2) 以下の値を状態変数に加算していく
+                    (- count 1)
+                    n))))
+(define (f2 n)
+  (iter 0 3 n))
+
+(f2 2)
+(f2 3)
+(f2 4)
+
